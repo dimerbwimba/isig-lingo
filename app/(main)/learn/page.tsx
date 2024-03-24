018fd824ffed093cf1a5e54e06d13ed49719a94e
dimerbwimba <dimerbwimba191996@gmail.com>
2024-03-24 19:24:54 +0100

    getCoursePRogress | getLessonPercentage | Front End Update

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -4,21 +4,30 @@ import {FeedWrapper} from "@/components/feed-wrapper";
 import {StickyWrapper} from "@/components/sticky-wrapper";
 import { Header } from "./_components/hearder";
 import {UserProgress} from "@/components/user-progress";
-import { getUnits, getUserProgress } from "@/db/queries";
+import { getCourseProgress, getLessonPercentage, getUnits, getUserProgress } from "@/db/queries";
 import { Unit } from "./_components/unit";
 
 const Learn = async () => {
     const userProgressData = getUserProgress();
+    const courseProgressData = getCourseProgress();
+    const lessonPercentageData = getLessonPercentage()
     const unitsData =  getUnits()
     const [
         userProgress,
         units,
+        courseProgress,
+        lessonPercentage
     ] = await Promise.all([
         userProgressData,
-        unitsData
+        unitsData,
+        courseProgressData,
+        lessonPercentageData
     ])
 
     if (!userProgress || !userProgress.activeCourse) {
+        redirect("/courses")
+    }
+    if (!courseProgress) {
         redirect("/courses")
     }
     return (
@@ -42,8 +51,8 @@ const Learn = async () => {
                                 description={ unit.description}
                                 title={ unit.title}
                                 lessons={unit.lessons}
-                                activeLesson={undefined}
-                                activeLessonPercentage={0}
+                                activeLesson={courseProgress.activeLesson}
+                                activeLessonPercentage={lessonPercentage}
                             />
                         </div>
                     ))
